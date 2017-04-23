3d8a2256c757e0717012801a06c4d6d76eadd1bd
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-04-23 10:24:50 -0400

    update solution for question two

--- main.js
+++ main.js
@@ -24,4 +24,23 @@
 
     // 2. IS UNIQUE: Implement an algorithm to determine if a string has all unique characters.
     // Bonus: What is you cannot use additional data structures?
-    // Hints: #44, #117, #132 
+    // Hints: #44, #117, #132
+
+    boolean isUniqueChars(String str) {
+      if (str.length() > 128) return false;
+
+      boolean[] char_set = new boolean[128];
+      for (int i = 0; i < str.length(); i++) {
+        int val = str.charAt(i);
+        if (char_set[val]) {
+          return false;
+        }
+        char_set[val] = true;
+      }
+      return true;
+    }
+//  You can also solve this problem by comparing each character of the string to every other character of the string //
+
+
+    // CharAt()
+    // This returns the specified character from a string
