f1a6fe1969f61b85b305be285d7dadfa78fa86f9
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-05-29 15:58:23 -0400

    update

--- main.js
+++ main.js
@@ -2366,3 +2366,118 @@ function oddEven(string) {
 }
 
 oddEven('hello')
+
+
+// Print an array
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+  }
+}
+
+printArray([4,5,6])
+
+
+// reverse a string
+
+function reverseString(string) {
+  console.log(string.split('').reverse().join(''));
+}
+
+reverseString('hello')
+
+// reverse a string
+
+function reverseString(string) {
+  let reverse = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string[i];
+  }
+  return reverse;
+}
+
+reverseString('hello')
+
+// isPalindrome
+
+function isPalindrome(string) {
+ if (string === string.split('').reverse().join('')) {
+   console.log('true');
+ } else {
+   console.log('false');
+ }
+}
+
+isPalindrome('hello')
+
+// isPalindrome
+
+
+function isPalindrome(string) {
+  let reverse = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+      reverse += string[i];
+  } if (string === reverse) {
+    console.log('true');
+  } else {
+    console.log('false');
+  }
+}
+
+isPalindrome('hello')
+
+
+// repeatify
+function repeatify(string, number) {
+  return string.repeat(number);
+}
+
+repeatify('hello', 3)
+
+// repeatify function
+
+function repeatify(string, number) {
+  let repeat = '';
+
+  for (let i = 0; i < number; i++) {
+      repeat += string;
+  }
+  return repeat;
+}
+
+repeatify('hello', 3)
+
+// Find the largest number
+
+function largestNumber(array) {
+  let largest = array[0];
+
+  array.forEach((d) => {
+    if (d > largest) { largest = d}
+  })
+  return largest;
+}
+
+largestNumber([4,5,6,7])
+
+
+// odd/Even number
+
+function oddNumber(string) {
+  let even = '';
+  let odd = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      even += string[i];
+    } else {
+      odd += string[i];
+    }
+  }
+  return `${even}, ${odd}`;
+}
+
+oddNumber('hello')
