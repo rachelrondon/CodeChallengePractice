771faaefc1a3662e84c4e4836a5a18222a81c516
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-06-07 14:46:22 -0400

    twoSum function

--- main.js
+++ main.js
@@ -4257,3 +4257,15 @@ findDifference([1,2,3])
 
 // twoSum
 // Write a function called twoSum that given an array of integers and a target number, returns two array integers that add up to the target.
+
+function twoSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++){
+      if (array[i] + array[j] === target) {
+        return `${array[i]}, ${array[j]}`;
+      }
+    }
+  }
+}
+
+twoSum([1,2,3,5,10,12], 4)
