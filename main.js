a34e53135772802da0ca00476b10190b18cd59d3
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-06-20 13:54:21 -0400

    update

--- main.js
+++ main.js
@@ -5124,3 +5124,196 @@ function isPrime(num) {
 }
 
 isPrime(7)
+
+// print an array
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+  }
+}
+
+printArray([1,2,3])
+
+// reverse a string
+
+function reverseString(string) {
+  return string.split('').reverse().join('');
+
+}
+
+reverseString('hello')
+
+//
+
+function reverseString(string) {
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+    }
+  return newString;
+}
+
+reverseString('hello')
+
+//
+
+function isPalindrome(string) {
+  if (string === string.split('').reverse().join("")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isPalindrome('madam')
+
+//
+
+function repeatify(string, number) {
+  return string.repeat(number);
+
+}
+
+repeatify('hello', 3)
+
+//
+
+function repeatify(string, number) {
+  let newString = '';
+
+  for (let i = 0; i < number; i++) {
+    newString += string;
+  }
+ return newString;
+}
+
+repeatify('hello', 3)
+
+
+// function findLargest
+
+function findLargestNumber(array) {
+  let largest = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+      largest = array[i];
+    }
+  }
+  return largest;
+}
+
+findLargestNumber([1,2,12,10,15])
+
+// oddsEvens
+
+
+function oddEven(string) {
+  let odd = '';
+  let even = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if ( i % 2 === 0) {
+      even += string[i];
+    } else {
+      odd += string[i];
+    }
+  }
+  return `${even}, ${odd}`;
+}
+
+oddEven('hello')
+
+// simple array sum
+
+
+function findSum(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+findSum([1,2,3])
+
+
+// findDifference
+// find the largest difference between two numbers within an array
+
+
+function findDifference(array) {
+  let largest = array[0];
+  let smallest = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > largest) {
+      largest = array[i]
+    } else if (array[i] < smallest ) {
+      smallest = array[i]
+    }
+  }
+  return largest - smallest;
+}
+
+findDifference([1,2,3,5,7])
+
+
+// twoSum
+// Write a function called twoSum that given an array of integers and a target number, returns two array integers that add up to the target.
+
+
+function twoSum(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        console.log(array[i], array[j]);
+      }
+    }
+  }
+}
+
+twoSum([1,2,3], 5)
+
+
+// isPrime
+
+function isPrime(num) {
+  if ( num === 2) {
+    return true;
+  } else if ( num > 1) {
+    for (let i = 2; i < num; i++) {
+      if ( num % i !== 0) {
+        return true;
+      } else if ( num === i * i ) {
+        return false
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+isPrime(4)
+
+// plusMinus
+// Given an array of integers,
+//calculate which fraction of its elements are positive, negative, and zeros.
+
+function plusMinus(size, array) {
+  let positive = 0;
+  let negative = 0;
+  let zeros = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0 ) {
+      positive += array[i];
+    } else if ()
+  }
+
+}
+
+plusMinus(4, [1,2,0,-1])
