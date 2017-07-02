fd24663bf899185fe71ad1d8bbec10ced2316522
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-07-01 20:30:38 -0400

    Fibonacci Sequence

--- main.js
+++ main.js
@@ -472,3 +472,234 @@ filterFunction([1,2,3,4])
 
 
 // reduce function
+
+var total = [1,2,3,4].reduce(function(a, b) {
+  return a + b;
+
+}, 0);
+
+// reverse
+
+function reverseString(string) {
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+  return newString;
+}
+
+reverseString('hello')
+
+
+// indexOf
+// syntax: str.indexOf(searchValue[, fromIndex])
+
+'Hello Rachel'.indexOf('Rachel')
+// this states that index number where 'Rachel' is listed
+
+[1,2,3].indexOf(1);
+[1,2,3].indexOf(4);
+
+
+// isPalindrome
+// write a function to check if the string is a palindrome
+
+function isPalindrome(string) {
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  } if (string === newString) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isPalindrome('hello')
+
+// write the isPalindrome function with the JS built-in reverse function
+function isPalindrome(string) {
+  if (string === string.split('').reverse().join('')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+isPalindrome('racecar')
+
+// missing
+
+// write a function and pass it an array
+// for this function, I am subtracting the actual sum from the expected sum;
+function findMissing(array) {
+  // define n as the length of the array plus one
+  let n = array.length + 1;
+  // let n = 4
+  // define the sum as zero
+  let sum = 0;
+  // 4 * (4 + 1) / 2
+  // 4 * 5  / 2
+  // 20 / 2 = 10
+  let expectedSum = n * (n + 1) / 2;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    // sum = 8
+  }
+  return expectedSum - sum;
+  // 10 - 8 = 2
+}
+
+findMissing([1,3,4])
+// 2 is the missing number
+
+// write a function that runs in 0(n) time
+
+
+
+// isBalanced
+// write a function that takes a string and returns true or false indicating whether its curly braces are balanced
+
+function isBalanced(string) {
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '{' || string[i] === '}') {
+      count += 1;
+    }
+  }
+  if (count % 2 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+isBalanced('foo { bar { baz } boo }')
+
+
+// isBalanced2
+// it should be able to look at braces that are curly {}, square [], or ();
+function isBalanced(string) {
+  let count = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === '{' || string[i] === '}') {
+      count += 1;
+    } else if (string[i] === '[' || string[i] === ']') {
+      count += 1;
+    } else if (string[i] === '(' || string[i] === ')') {
+      count += 1;
+    }
+  }
+  if (count % 2 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+isBalanced('(foo { bar (baz) [boo] })')
+// Look into ('foo { (bar [baz] } )')  ;
+// A string with interleaving braces should return false;
+
+// uniq
+// Writes a function that takes an array of numbers, and returns the unique numbers.
+// Can you do this in O(N) time?
+// Work on the isUnique function
+
+
+// isPrime
+
+function isPrime(num) {
+  if (num === 2) {
+    return true;
+  } else if (num > 1) {
+    for (let i = 2; i < num; i++) {
+      if ( num % i !== 0) {
+        return true;
+      } else if ( num = i * i) {
+        return false;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+isPrime(4)
+
+// factorial
+
+function factorialize(num) {
+  // if the number is less than 0; then the factor is -1
+  if (num < 0) {
+    return -1;
+  // if the number is 0; then the factor is 1
+  } else if (num === 0) {
+    return 1;
+// else; if the number is not a negative number or zero
+// then retun the number multiplized by (factorialize(num - 1))
+  } else {
+    return (num * factorialize(num - 1));
+  }
+}
+
+factorialize(6)
+
+
+// fibonacci
+
+function fibIterative(n) {
+  let [a,b] = [1,0];
+  while (n-- > 0) {
+    [a,b] = [b + a, a];
+  }
+  return b;
+}
+
+fibIterative(20)
+
+
+//
+
+// this is written in ES6
+function fibIterative(n) {
+  let [a,b] = [1,0];
+
+// in every interation of the loop,
+// counting back from n to 0;
+// a becomes the sum of the two numbers
+// and b becomes the previous value of the higher number a
+// When n reaches 0, the lower of the two numbers is returned and it resolves to the n th number in the Fibonacci sequence
+
+// the loop is iterating down from n to 0
+  while (n-- > 0) {
+    [a,b] = [b + a, a];
+  }
+  return b;
+}
+
+fibIterative(20)
+
+// source: https://gregjs.com/javascript/2016/writing-a-fibonacci-implementation-in-javascript/
+
+
+// here is the solution in ES5
+
+function fibIterativeES5(n) {
+  let a = 1;
+  let b = 0;
+  let c = null;
+
+  while (n > 0) {
+    c = a;
+    a = b + a;
+    b = c;
+    n--;
+  }
+  return b;
+}
