3b4539b12ab3a1bf2e244f9cc9f6234efdde21dc
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-09-25 18:58:02 -0400

    add valid phone number question

--- main.js
+++ main.js
@@ -3460,3 +3460,29 @@ function findDifference(array) {
 findDifference([1,2,3,10])
 
 
+/* Write a function that accepts a string, and returns true if it is in the form of a phone number.
+Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+
+Only worry about the following format:
+(123) 456-7890 (don't forget the space after the close parentheses)
+
+Examples:
+
+validPhoneNumber("(123) 456-7890")  =>  returns true
+validPhoneNumber("(1111)555 2345")  => returns false
+validPhoneNumber("(098) 123 4567")  => returns false */
+
+
+// check to see if the string is in the form of a phone number
+
+
+function validPhoneNumber(phoneNumber){
+  var regEx = '^\s*(\d{3})\s*(\d{3})[-. ]*(\d{4})\s*$'
+      if ('phoneNumber === regEx') {
+        return true;
+      } else {
+        return false;
+      }
+}
+
+validPhoneNumber('(777) 777-7777')
